5f8ece2a14c62840c34902a18605c84b7003431e
vicky sharma <vickysharma567890@gmail.com>
2026-05-30 15:39:46 +0530

    all done except gallery

--- header.php
+++ header.php
@@ -1,26 +1,43 @@
-<header id="header_main" class="header header-fixed style-absolute">
-                <div class="themesflat-container">
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="header-top">
-                                <div class="wg-information location">
-                                    <div class="icon">
-                                        <i class="icon-location"></i>
+<header id="header_main" class="header-2 header-fixed header">
+               
+                <div class="top">
+                    <div class="themesflat-container">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="header-top">
+                                    <div class="information">
+                                        <div class="wg-information location">
+                                            <div class="icon">
+                                                <i class="icon-location"></i>
+                                            </div>
+                                            <div class="content">
+                                                <p>LOCATION</p>
+                                                <p>292, KG-1, M Block Market, Vikaspuri, New Delhi</p>
+                                            </div>
+                                        </div>
+                                        <div class="wg-information call">
+                                            <div class="icon">
+                                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="16" height="18" viewBox="0 0 16 18">
+                                                    <image   data-name="" width="16" height="18" xlink:href="data:img/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAAzklEQVQ4jZVTURXCMAy88GagFmYBC9OCBbRMAxKYBJDAJDAJxytr8rLSsfZ+9toll7skhYJkT/LFOjxIBniQHCuTFdeYLliT7wAGtGEBMEmS8m5MNqiCC4BQl7LBLK6JQ6ONm4g87RRVNDbxW+zkGOdG+bGJ6PKLhCk7K4KzuS2YFknRl0pG2Rqgd96Cr1g9ESMQkcWR7BGc09e6f8oClKBooaS22/kRfh7LlrjU4PVNVO7AeGThCBaXE8T518Di7C04G9HnvzEuIrIuEYAP95cHumv/LXMAAAAASUVORK5CYII="/>
+                                                </svg>
+                                            </div>
+                                            <div class="content">
+                                                <p>CALL DELIVERY</p>
+                                                <p>+91 9899591067</p>
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div class="content">
-                                        <div class="title">LOCATION</div>
-                                        <p>292, KG-1, M Block Market, Vikaspuri, New Delhi</p>
-                                    </div>
-                                </div>
-                                <div class="wg-information call row-reverse text-end">
-                                    <div class="icon">
-                                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="16" height="18" viewBox="0 0 16 18">
-                                            <image   data-name="" width="16" height="18" xlink:href="data:img/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAAzklEQVQ4jZVTURXCMAy88GagFmYBC9OCBbRMAxKYBJDAJDAJxytr8rLSsfZ+9toll7skhYJkT/LFOjxIBniQHCuTFdeYLliT7wAGtGEBMEmS8m5MNqiCC4BQl7LBLK6JQ6ONm4g87RRVNDbxW+zkGOdG+bGJ6PKLhCk7K4KzuS2YFknRl0pG2Rqgd96Cr1g9ESMQkcWR7BGc09e6f8oClKBooaS22/kRfh7LlrjU4PVNVO7AeGThCBaXE8T518Di7C04G9HnvzEuIrIuEYAP95cHumv/LXMAAAAASUVORK5CYII="/>
-                                        </svg>
-                                    </div>
-                                    <div class="content">
-                                        <div class="title">CALL DELIVERY</div>
-                                        <p>+91 9899591067</p>
+                                    <div class="widget-social style-4">
+                                        <ul class="">
+                                            <li><a href="#" class="icon-fb"></a></li>
+                                            <li>
+                                                <a href="#" class="">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns" xmlns:xlink="http://www.w3.org/1999/xlink" height="60px" version="1.1" viewBox="0 0 60 60" width="60px"><title/><desc/><defs/><g fill="none" fill-rule="evenodd" id="black" stroke="none" stroke-width="1"><g id="slice" transform="translate(-600.000000, -300.000000)"/><g fill="#000000" id="trip_advisor" transform="translate(9.000000, 17.000000)"><path d="M40.4891264,7.95839112 C40.8955786,6.73786408 41.5174295,5.30022405 42.3763774,4.43443935 L35.2962751,4.43443935 C35.1793143,4.39816494 35.057061,4.37362637 34.9379832,4.3410861 C34.5532296,3.73295636 31.9197575,0.307692308 21.3943371,0.307692308 C9.84325974,0.307692308 7.79617978,4.43443935 7.79617978,4.43443935 L0.411767511,4.43443935 C1.35380534,5.38397525 2.00846871,7.01792382 2.4090994,8.30246453 C1.0484369,10.1215193 0.230769231,12.3758669 0.230769231,14.8249227 C0.230769231,20.8171343 5.06744548,25.6923077 11.0128685,25.6923077 C14.3084132,25.6923077 17.2594261,24.190654 19.238764,21.8338846 L21.6864747,25.2890217 L23.7991797,21.6999893 C25.7779884,24.1346421 28.7819248,25.6923077 32.1409776,25.6923077 C38.0858714,25.6923077 42.9230769,20.8171343 42.9230769,14.8249227 C42.9230769,12.2222341 42.0085593,9.83132402 40.4891264,7.95839112 Z M11.192572,23.3846154 C6.42073364,23.3846154 2.53846154,19.5028718 2.53846154,14.7310335 C2.53846154,9.95919518 6.42073364,6.07692308 11.192572,6.07692308 C15.9638817,6.07692308 19.8461538,9.95919518 19.8461538,14.7310335 C19.8461538,19.5028718 15.9638817,23.3846154 11.192572,23.3846154 Z M7.52079953,11.0592611 C6.53999788,12.0400627 6,13.3439083 6,14.7310345 C6,16.1181606 6.53999788,17.4220062 7.52079953,18.4022774 C8.50160117,19.383079 9.80544674,19.9230769 11.1925729,19.9230769 C14.0554104,19.9230769 16.3846154,17.5938719 16.3846154,14.7310345 C16.3846154,11.8676665 14.0554104,9.53846154 11.1925729,9.53846154 C9.80544674,9.53846154 8.50160117,10.0784594 7.52079953,11.0592611 Z M8.33650234,17.5865746 C7.57384377,16.823916 7.15384615,15.8096158 7.15384615,14.7310667 C7.15384615,13.6519227 7.57384377,12.6382173 8.33650234,11.8755588 C9.09975581,11.1123053 10.1134611,10.6923077 11.1926051,10.6923077 C13.4193064,10.6923077 15.2307692,12.5043654 15.2307692,14.7310667 C15.2307692,16.9577679 13.4193064,18.7692308 11.1926051,18.7692308 C10.1134611,18.7692308 9.09975581,18.3492331 8.33650234,17.5865746 C8.33650234,17.5865746 9.09975581,18.3492331 8.33650234,17.5865746 Z M9.46153846,14.7310302 C9.46153846,15.6868554 10.2367435,16.4615385 11.1925687,16.4615385 C12.1483939,16.4615385 12.9230769,15.6868554 12.9230769,14.7310302 C12.9230769,13.775205 12.1483939,13 11.1925687,13 C10.2367435,13 9.46153846,13.775205 9.46153846,14.7310302 Z M31.9615385,23.3846154 C27.1898459,23.3846154 23.3076923,19.5028718 23.3076923,14.7310335 C23.3076923,9.95919518 27.1898459,6.07692308 31.9615385,6.07692308 C36.7332311,6.07692308 40.6153846,9.95919518 40.6153846,14.7310335 C40.6153846,19.5028718 36.7332311,23.3846154 31.9615385,23.3846154 Z M28.2900303,11.0592611 C27.3092286,12.0400627 26.7692308,13.3439083 26.7692308,14.7310345 C26.7692308,16.1181606 27.3092286,17.4220062 28.2900303,18.4022774 C29.2708319,19.383079 30.5746775,19.9230769 31.9618037,19.9230769 C34.8246412,19.9230769 37.1538462,17.5938719 37.1538462,14.7310345 C37.1538462,11.8676665 34.8246412,9.53846154 31.9618037,9.53846154 C30.5746775,9.53846154 29.2708319,10.0784594 28.2900303,11.0592611 Z M29.1057331,17.5865746 C28.3430745,16.823916 27.9230769,15.8096158 27.9230769,14.7310667 C27.9230769,13.6519227 28.3430745,12.6382173 29.1057331,11.8755588 C29.8689866,11.1123053 30.8826919,10.6923077 31.9618359,10.6923077 C34.1885372,10.6923077 36,12.5043654 36,14.7310667 C36,16.9577679 34.1885372,18.7692308 31.9618359,18.7692308 C30.8826919,18.7692308 29.8689866,18.3492331 29.1057331,17.5865746 C29.1057331,17.5865746 29.8689866,18.3492331 29.1057331,17.5865746 Z M30.2307692,14.7310302 C30.2307692,15.6868554 31.0059743,16.4615385 31.9617995,16.4615385 C32.9176247,16.4615385 33.6923077,15.6868554 33.6923077,14.7310302 C33.6923077,13.775205 32.9176247,13 31.9617995,13 C31.0059743,13 30.2307692,13.775205 30.2307692,14.7310302 Z M21.3607364,1.46153846 C28.6443758,1.46153846 31.3846154,4.54778963 31.3846154,4.54778963 C31.3846154,4.54778963 23.5961538,3.22518656 21,13 C18.4038462,3.22518656 10.6153846,4.54778963 10.6153846,4.54778963 C10.6153846,4.54778963 13.3561463,1.46153846 20.6397856,1.46153846 L21.3607364,1.46153846 Z"/></g></g></svg>
+                                                </a>
+                                            </li>
+                                            <li><a href="#" class="icon-envelope"></a></li>
+                                            <li><a href="#" class="icon-instagram"></a></li>
+                                        </ul>
                                     </div>
                                 </div>
                             </div>
@@ -28,15 +45,22 @@
                     </div>
                 </div>
                 <div class="header-inner"> 
-                    <div class="header-inner-wrap">
+                    <div class="header-inner-wrap"> 
                         <div class="mobile-button">
                             <span></span>
                         </div>
-                        
-                        <nav class="main-nav left">
+                        <!-- /mobile-button -->
+                        <nav class="main-nav">
                             <ul class="menu-primary-menu">
-                                <li class="menu-item">
-                                    <a href="index.php">Home</a>
+                                 
+                        <li class="menu-item menu-item-has-children current-menu-item">
+                                    <a href="index.html" rel="home" class="main-logo">
+                            <img id="logo_header" alt="" src="assets/images/logo/SV_logo1.png"
+                            data-retina="assets/images/logo/SV_logo1.png" style="width:180px;" >
+                        </a>   
+                                </li>
+                                <li class="menu-item menu-item-has-children current-menu-item">
+                                    <a href="index.php">Home</a> 
                                 </li>
                                 <li class="menu-item">
                                     <a href="about.php">About Us</a>
@@ -44,20 +68,8 @@
                                 <li class="menu-item">
                                     <a href="menu.php">Menu</a>
                                 </li>
-                            </ul>
-                        </nav><!-- /main-nav -->
-                        <div id="site-logo">
-                            <div id="site-logo-inner">
-                                <a href="index.php" rel="home" class="main-logo">
-                                    <img id="logo_header" alt="" src="assets/images/logo/logo2.png"
-                                    data-retina="assets/images/logo/main_logo.png" style="width: auto; height: auto;">
-                                </a>
-                            </div>
-                        </div><!-- /logo -->
-                        <nav class="main-nav right">
-                            <ul class="menu-primary-menu">
-                               <li class="menu-item">
-                                    <a class="item-menu-mobile" href="gallery.php">Gallery</a>
+                                <li class="menu-item menu-item-has-children">
+                                    <a href="gallery.php">Gallery</a>
                                 </li>
                                 <li class="menu-item menu-item-has-children">
                                     <a href="blog.php">Blog</a>
@@ -67,33 +79,34 @@
                                 </li>
                             </ul>
                         </nav><!-- /main-nav -->
+                        
                     </div>
                 </div>
                 <div class="mobile-nav-wrap">
                     <div class="overlay-mobile-nav"></div>
                     <div class="inner-mobile-nav">
-                        <a href="index.php" rel="home" class="main-logo">
-                            <img id="mobile-logo_header" alt="" src="assets/images/logo/logo2.png" data-retina="assets/images/logo/logo2.png">
+                        <a href="index.html" rel="home" class="main-logo">
+                            <img id="mobile-logo_header" alt="" src="assets/images/logo/SV_logo1.png" data-retina="assets/images/logo/SV_logo1.png">
                         </a>
                         <div class="mobile-nav-close">
                             <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="white" x="0px" y="0px" width="20px" height="20px" viewBox="0 0 122.878 122.88" enable-background="new 0 0 122.878 122.88" xml:space="preserve"><g><path d="M1.426,8.313c-1.901-1.901-1.901-4.984,0-6.886c1.901-1.902,4.984-1.902,6.886,0l53.127,53.127l53.127-53.127 c1.901-1.902,4.984-1.902,6.887,0c1.901,1.901,1.901,4.985,0,6.886L68.324,61.439l53.128,53.128c1.901,1.901,1.901,4.984,0,6.886 c-1.902,1.902-4.985,1.902-6.887,0L61.438,68.326L8.312,121.453c-1.901,1.902-4.984,1.902-6.886,0 c-1.901-1.901-1.901-4.984,0-6.886l53.127-53.128L1.426,8.313L1.426,8.313z"/></g></svg>
                         </div>
                         <nav id="mobile-main-nav" class="mobile-main-nav">
                             <ul id="menu-mobile-menu" class="menu">
-                                <li class="menu-item menu-item-has-children-mobile current-menu-item">
-                                    <a class="item-menu-mobile" href="index.php">Home</a>
+                                 <li class="menu-item menu-item-has-children current-menu-item">
+                                    <a href="index.php">Home</a> 
                                 </li>
                                 <li class="menu-item">
-                                    <a class="item-menu-mobile" href="about.php">About Us</a>
+                                    <a href="about.php">About Us</a>
                                 </li>
                                 <li class="menu-item">
-                                    <a class="item-menu-mobile" href="menu.php">Menu</a>
+                                    <a href="menu.php">Menu</a>
                                 </li>
-                                <li class="menu-item">
-                                    <a class="item-menu-mobile" href="gallery.php">Gallery</a>
+                                <li class="menu-item menu-item-has-children">
+                                    <a href="gallery.php">Gallery</a>
                                 </li>
-                                <li class="menu-item menu-item-has-children-mobile">
-                                    <a class="item-menu-mobile" href="blog.php">Blog</a>
+                                <li class="menu-item menu-item-has-children">
+                                    <a href="blog.php">Blog</a>
                                 </li>
                                 <li class="menu-item">
                                     <a href="contact.php">Contact</a>
